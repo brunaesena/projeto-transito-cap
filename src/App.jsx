742c3258f0dd5cd3b4b46ec71713be68b65014b4
brunaesena <bruna_ewelin@hotmail.com>
2024-03-14 21:01:19 -0300

    subindo primeira versao

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar />
+      <h1>Conteúdo do App</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Página Inicial</a></li>
+        <li><a href="#">Reportar acidente</a></li>
+        <li><a href="#">Ver acidentes</a></li>
+        <li><a href="#">Estatisticas</a></li>
+        <li><a href="#">Sobre</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
